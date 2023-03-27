036604f42f65c106dc8f7b6cf20387697d0191d7
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-03-27 14:40:15 +0700

    memasukkan string getstatus

--- exrcs/exrcs.cpp
+++ exrcs/exrcs.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+string getStatus(int matematika, int bahasa_inggris) {  // fungsi untuk menentukan status diterima atau tidak
+    string status;
+    float rata_rata = (matematika + bahasa_inggris) / 2.0;
+    if (rata_rata >= 70 || matematika > 80) {
+        status = "diterima";
+    }
+    else {
+        status = "ditolak";
+    }
+    return status;
+}
