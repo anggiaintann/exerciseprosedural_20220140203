2de983f640fc462cb9583987db7a19b0f9224598
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-03-27 14:45:03 +0700

    inisialisasi data

--- exrcs/exrcs.cpp
+++ exrcs/exrcs.cpp
@@ -15,3 +15,7 @@ string getStatus(int matematika, int bahasa_inggris) {  // fungsi untuk menentuk
     }
     return status;
 }
+int main() {        // inisialisasi data
+    string nama[] = { "asroni", "lia", "joko", "bayu", "ali", "putri", "putra", "dea", "bagas", "kiki", "deni", "fajar", "nila", "agus", "putri", "dina", "rizal", "rani", "roni", "tika" };
+    int matematika[] = { 75, 80, 65, 90, 70, 80, 60, 85, 75, 90, 85, 75, 80, 70, 65, 75, 85, 70, 80, 75 };
+    int bahasa_inggris[] = { 80, 85, 70, 90, 75, 85, 65, 90, 80, 95, 90, 80, 85, 75, 70, 80, 90, 75, 85, 80 };
